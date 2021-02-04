65415da0705b8a7e150254d9cf8f6d553c97d087
Val <valentin.guiller@gmail.com>
2021-02-04 19:45:31 +0100

    add comment

--- Assets/CharacterMotor.cs
+++ Assets/CharacterMotor.cs
@@ -7,7 +7,7 @@ public class CharacterMotor : MonoBehaviour {
     // Animations du perso
     Animation animations;
 
-    // Vitesse de deplacement
+    // Vitesse de deplacement dknciebeo
     public float walkSpeed;
     public float runSpeed;
     public float turnSpeed;
